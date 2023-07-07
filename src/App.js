c0b3987c0ab80d63e6240400d7cfa5999bc8c854
vchirikov <vovanchig@gmail.com>
2023-07-07 14:16:07 +0300

    add repro for issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import TypesafeI18n, { useI18nContext } from './i18n/i18n-react';
+import { useEffect, useState } from 'react';
+
+const TestComponent = () => {
+  const { setLocale, locale } = useI18nContext();
+
+  const [state, setState] = useState('en');
+
+  // setState is safe to use as deps
+  useEffect(() => {
+    setState('de');
+    return () => setState('en');
+  }, [setState]);
+
+  // but setLocale is not :\
+  // Warning: Maximum update depth exceeded. This can happen when a component calls setState inside useEffect, but useEffect either doesn't have a dependency array, or one of the dependencies changes on every render.
+  useEffect(() => {
+    setLocale('de');
+    return () => setLocale('en');
+  }, [setLocale]);
+
+
+  return <>Current locale is {locale} <br />Current state is {state}</>;
+};
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TypesafeI18n locale='en'>
+      <TestComponent />
+    </TypesafeI18n>
   );
 }
 
